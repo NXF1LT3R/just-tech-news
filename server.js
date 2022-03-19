e4f12ae6a7f1c6b6aa336d950cd61b2b18939bbf
NXF1LT3R <eplecha87@gmail.com>
2022-03-19 16:46:07 -0400

    Optimize and clean up application

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,7 +19,11 @@ const sess = {
   })
 };
 
-const hbs = exphbs.create({});
+app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -27,7 +31,6 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session(sess));
 
 app.use(require('./controllers/'));
 
